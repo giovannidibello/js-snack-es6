022ee5ff8f05c02eff8cc975d914f5220b2094be
Giovanni <dibellogio@gmail.com>
2025-01-16 15:49:24 +0100

    add logic Snack 2

---
+++ Snack 2/script.js
@@ -0,0 +1,15 @@
+// Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti. Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+// Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+// Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+// creo un array di oggetti con le squadre di calcio
+
+// aggiungo le proprietà compilando solo il nome
+
+// creo una funzione che genera un numero random
+
+// richiamo la funzione per riempire i valori dei punti fatti e falli subiti
+
+// creo un array che prende dal precedente solo nome e falli subiti delle squadre
+
+// stampo l'array in console
