c3e00feb05b2773e16cbfe089551b8566b7e58af
Giovanni <dibellogio@gmail.com>
2025-01-16 16:32:55 +0100

    add logic Snack 3

---
+++ Snack 3 (Bonus)/script.js
@@ -0,0 +1,9 @@
+// Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b). La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
+
+// creo un array con dei numeri
+
+// creo una funzione con tre argomenti un array e due numeri (a più piccolo di b)
+
+// cerco i numeri compresi tra a e b nell'array fornito
+
+// metto i numeri compresi in un nuovo array
