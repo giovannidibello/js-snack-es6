55d3f8b0910a472e3ed925b7e61d5253c02c625d
Giovanni <dibellogio@gmail.com>
2025-01-16 15:43:51 +0100

    add js Snack 1

--- Snack 1/script.js
+++ Snack 1/script.js
@@ -2,8 +2,37 @@
 
 // creo un array di oggetti con le bici
 
-// imposto le proprietà delle bici
+const bici = [
+
+    // imposto le proprietà delle bici
+    { "nome": "Bianchi", "peso": 7.0 },
+    { "nome": "Look", "peso": 7.5 },
+    { "nome": "Willier Triestina", "peso": 7.6 },
+    { "nome": "Pinarello", "peso": 8.0 },
+    { "nome": "Colnago", "peso": 7.9 },
+    { "nome": "Cervelo S5", "peso": 7.8 },
+    { "nome": "Canyon", "peso": 7.1 },
+    { "nome": "Merida Reacto Team", "peso": 7.9 }
+];
+
+// // stampo l'array in console
+// console.table(bici);
+
+// inizializzo la variabile con il primo elemento dell'array
+let biciLeggera = bici[0];
 
 // ciclo l'array di oggetti
+for (let i=1; i < bici.length; i++){
 
-// confronto la proprietà peso e restituisco la minore
+    // confronto la proprietà peso
+    if (bici[i].peso < biciLeggera.peso){
+
+        // aggiorno il peso 
+        biciLeggera = bici[i];
+    }
+}
+
+//  restituisco quella con il peso minore
+console.log("La bici più leggera è:", biciLeggera.nome, "con un peso di", biciLeggera.peso, "kg");
+
+
