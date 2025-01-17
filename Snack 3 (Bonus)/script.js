9984e73f87bb2e940b0058e84d307b4ddb73a287
Giovanni <dibellogio@gmail.com>
2025-01-17 10:29:33 +0100

    fix js change, for misunderstanding trace

--- Snack 3 (Bonus)/script.js
+++ Snack 3 (Bonus)/script.js
@@ -1,46 +1,44 @@
 // Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b). La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
 
-// creo un array con dei numeri
-const arrayNumeri = [23, 45, 67, 12, 89, 34, 56, 78, 90, 11, 43, 22, 74, 18, 39, 65, 27, 88, 54, 32];
+// creo un array con delle stringhe
+const arrayStringhe = ["cane", "gatto", "topo", "lucertola", "mucca", "grillo"]
 
-console.log(arrayNumeri);
+console.log(arrayStringhe);
 
 // creo una funzione con tre argomenti, un array e due numeri (a più piccolo di b)
-function numeriTraMinMax (array, min, max){
-    
+function posizioneTraMinMax(array, min, max) {
+
     // inizializzo l'array
     const nuovoArray = [];
 
     // ciclo sull'intera lunghezza dell'array
-    for (let i=0; i < array.length; i++) {
+    for (let i = min; i <= max; i++) {
 
-        // seleziono il numero corrente
-        const numeroCorrente = array[i];
+        // seleziono la posizione corrente
+        const posizioneCorrente = array[i];
 
-        // cerco i numeri compresi tra a e b nell'array fornito
-        if (numeroCorrente >= min && numeroCorrente <= max) {
-            // metto i numeri compresi in un nuovo array
-            nuovoArray.push(numeroCorrente);
-        }
+        // metto i valori delle posizioni nel nuovo array
+        nuovoArray.push(posizioneCorrente);
+
 
     }
 
-    return nuovoArray;    
+    return nuovoArray;
 
 }
 
 // definisco dei min e max come esempio
-let numeroMin = 13;
-let numeroMax = 67;
+let numeroMin = 1;
+let numeroMax = 4;
 
 // log dei numeri scelti
-console.log(`Minimo:  ${numeroMin}  Massimo:  ${numeroMax}`);  
+console.log(`Minimo:  ${numeroMin}  Massimo:  ${numeroMax}`);
 
 // invoco la funzione
-const numerCompresi = numeriTraMinMax(arrayNumeri, numeroMin, numeroMax)
+const posizioneComprese = posizioneTraMinMax(arrayStringhe, numeroMin, numeroMax)
 
 // stampo l'output
-console.log(numerCompresi);
+console.log(posizioneComprese);
 
 
 
